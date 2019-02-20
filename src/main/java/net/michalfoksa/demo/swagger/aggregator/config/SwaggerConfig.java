6372573c981bd96dab38516bf29f0f1baffdeec5
Michal Foksa <mail@michalfoksa.net>
2019-02-20 11:06:54 +0100

    Embed service uri discovery into KubernetesUriResolver.

--- src/main/java/net/michalfoksa/demo/swagger/aggregator/config/SwaggerConfig.java
+++ src/main/java/net/michalfoksa/demo/swagger/aggregator/config/SwaggerConfig.java
@@ -19,28 +19,9 @@ public class SwaggerConfig {
     public SwaggerResourcesProvider swaggerResourcesProvider(DiscoveryClient discoveryClient,
             SwaggerResourceService resourceService) {
 
-        return () -> {
-
-            // return
-            // discoveryClient.getServices().stream().sorted(String::compareTo)
-            // .map(serviceId ->
-            // discoveryClient.getInstances(serviceId).stream().findAny()
-            // .map(pod -> resourceService.createResource(pod)).orElse(null))
-            // .filter(Objects::nonNull).collect(Collectors.toList());
-
-            return discoveryClient.getServices().stream().sorted(String::compareTo)
-                    .map(serviceName -> resourceService.createResource(serviceName))
-                    .filter(Objects::nonNull).collect(Collectors.toList());
-
-            // MyInstace[] array = { new MyInstace("bodyshop",
-            // "http://localhost:10081/"),
-            // new MyInstace("paintshop", "http://localhost:10082/"),
-            // new MyInstace("powertrain", "http://localhost:10083/")
-            // };
-            //
-            // return Arrays.stream(array).map(pod ->
-            // resourceService.createResource(pod)).collect(Collectors.toList());
-        };
+        return () -> discoveryClient.getServices().stream().sorted(String::compareTo)
+                .map(serviceName -> resourceService.createResource(serviceName)).filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
 }
