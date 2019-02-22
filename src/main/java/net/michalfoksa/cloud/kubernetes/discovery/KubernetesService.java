67fbc38cada9f4602462a70c26256288064d5de2
Michal Foksa <mail@michalfoksa.net>
2019-02-22 07:59:04 +0100

    Draft of KubernetesServiceDiscoveryClient.

---
+++ src/main/java/net/michalfoksa/cloud/kubernetes/discovery/KubernetesService.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.michalfoksa.cloud.kubernetes.discovery;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+import io.fabric8.kubernetes.api.model.ServicePort;
+
+public class KubernetesService implements ServiceInstance {
+
+    private static final String HTTP_PREFIX = "http://";
+    private static final String HTTPS_PREFIX = "https://";
+    private static final String COLN = ":";
+
+    private final String type;
+    private final String serviceId;
+    private final String namespace;
+    private final String clusterIP;
+    private final String externalName;
+    private final ServicePort servicePort;
+    private final Boolean secure;
+    private final Map<String, String> metadata;
+
+
+    public KubernetesService(String serviceId, String namespace, String type, String clusterIP, String externalName,
+            ServicePort servicePort, Map<String, String> metadata, Boolean secure) {
+
+        Assert.hasText(type, "[Assertion failed] - type must not be null or empty");
+
+        if ( StringUtils.isEmpty(clusterIP) && StringUtils.isEmpty(externalName) ) {
+            throw new NullPointerException("clusterIP or externalName must not be empty");
+        }
+
+        this.serviceId = serviceId;
+        this.namespace = namespace;
+        this.type = type;
+        this.clusterIP = clusterIP;
+        this.externalName = externalName;
+        this.servicePort = servicePort;
+        this.metadata = metadata;
+        this.secure = secure;
+    }
+
+    @Override
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    @Override
+    public String getInstanceId() {
+        return namespace + "/" + serviceId;
+    }
+
+    @Override
+    public String getHost() {
+        if ("ClusterIP".equals(type)) {
+            return clusterIP;
+        }
+
+        return externalName;
+    }
+
+    @Override
+    public int getPort() {
+        return servicePort.getPort();
+    }
+
+    @Override
+    public boolean isSecure() {
+        return secure;
+    }
+
+    @Override
+    public URI getUri() {
+        StringBuilder sb = new StringBuilder();
+
+        if (isSecure()) {
+            sb.append(HTTPS_PREFIX);
+        } else {
+            sb.append(HTTP_PREFIX);
+        }
+
+        sb.append(getHost()).append(COLN).append(getPort());
+        try {
+            return new URI(sb.toString());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Map<String, String> getMetadata() {
+        return metadata;
+    }
+}
+
---
+++ src/main/java/net/michalfoksa/cloud/kubernetes/discovery/KubernetesServiceDiscoveryClient.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.michalfoksa.cloud.kubernetes.discovery;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.kubernetes.discovery.KubernetesClientServicesFunction;
+import org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryClient;
+import org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryProperties;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.SimpleEvaluationContext;
+import org.springframework.util.Assert;
+
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServicePort;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+/***
+ * Discovery client providing methods to retrieve Kubernetes services as if they
+ * were service instances in context of Spring cloud discovery.
+ *
+ * I.e.: services might be returned instead of service instances.
+ *
+ * @author Michal Foksa
+ *
+ */
+public class KubernetesServiceDiscoveryClient extends KubernetesDiscoveryClient {
+
+    private final KubernetesDiscoveryProperties properties;
+
+    private final KubernetesClientServicesFunction kubernetesClientServicesFunction;
+    private final SpelExpressionParser parser = new SpelExpressionParser();
+    private final SimpleEvaluationContext evalCtxt = SimpleEvaluationContext.forReadOnlyDataBinding()
+            .withInstanceMethods().build();
+
+    KubernetesServiceDiscoveryClient(KubernetesClient client, KubernetesDiscoveryProperties kubernetesDiscoveryProperties,
+            KubernetesClientServicesFunction kubernetesClientServicesFunction) {
+
+        super(client, kubernetesDiscoveryProperties, kubernetesClientServicesFunction);
+
+        this.properties = kubernetesDiscoveryProperties;
+        this.kubernetesClientServicesFunction = kubernetesClientServicesFunction;
+    }
+
+    @Override
+    public String description() {
+        return "Kubernetes Discovery Client V2";
+    }
+
+    @Override
+    public List<ServiceInstance> getInstances(String serviceId) {
+        Assert.notNull(serviceId, "[Assertion failed] - the object argument must not be null");
+
+        return new ArrayList<>(getKubernetesServices());
+    }
+
+    public List<KubernetesService> getKubernetesServices() {
+        String spelExpression = properties.getFilter();
+        Predicate<Service> filteredServices = (Service instance) -> true;
+        if (spelExpression == null || spelExpression.isEmpty()) {
+            filteredServices = (Service instance) -> true;
+        } else {
+            Expression filterExpr = parser.parseExpression(spelExpression);
+            filteredServices = (Service instance) -> {
+                Boolean include = filterExpr.getValue(evalCtxt, instance, Boolean.class);
+                if (include == null) {
+                    return false;
+                }
+                return include;
+            };
+        }
+        return getKubernetesServices(filteredServices);
+    }
+
+    public List<KubernetesService> getKubernetesServices(Predicate<Service> filter) {
+        return kubernetesClientServicesFunction.apply(getClient()).list().getItems().stream()
+                .filter(filter).map(s -> {
+                    ServicePort port = s.getSpec().getPorts().stream().findFirst().orElseThrow(IllegalStateException::new);
+                    s.getMetadata().getNamespace();
+                    return new KubernetesService(s.getMetadata().getName(), s.getMetadata().getNamespace(),
+                            s.getSpec().getType(), s.getSpec().getClusterIP(), s.getSpec().getExternalName(), port,
+                            new HashMap<String, String>(), false);
+                }).collect(Collectors.toList());
+    }
+
+}
